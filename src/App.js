527a4ab7d630e4decab56705614d73004ead1adf
Sarthik-mehta <65113721+Sarthik-mehta@users.noreply.github.com>
2022-07-12 17:53:42 +0530

    added login and register page.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
+import { useState } from 'react';
 import{Provider} from "react-redux";
 import store from "./store";
+import { useHistory } from "react-router-dom"
 import {BrowserRouter as Router,Route} from "react-router-dom";
 import Nav from "./components/Nav"
 import Home from "./components/Home";
@@ -9,28 +11,35 @@ import Details from './components/Details';
 import Footer from './components/Footer';
 import About from './components/About';
 import Contact from './components/Contact';
-
-
+import Login from './components/login/login';
+import Register from './components/register/register';
 
 
 function App() {
+
+  // const [ user, setLoginUser] = useState({})
   return (
     <Router>
       <Provider store={store}>
       
      <Nav/>
-     <Route path="/" exact component={Home}/>
+     <Route path="/" exact>
+      <Home/>
+     </Route>
      <Route path="/cart" exact component={Cart}/>
      <Route path="/about" exact component={About}/>
      <Route path="/contact" exact component={Contact}/>
      <Route path="/details/:id" exact component={Details}/>
+     <Route path="/login" exact>
+              <Login/>
+      </Route>
+     <Route path="/register" exact component={Register}/>
      <Footer/>
      {/* <Googlepay/> */}
      </Provider>
-     
+
      </Router>
-     
-   
+
   );
 }
 
